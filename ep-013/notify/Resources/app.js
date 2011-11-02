f9d59cdd794d19edd959a0350836dfd571c465fc
tonylukasavage <anthony.lukasavage@gmail.com>
2011-11-02 12:38:31 -0400

    added comments

--- ep-013/notify/Resources/app.js
+++ ep-013/notify/Resources/app.js
@@ -1,3 +1,7 @@
+// The function we'll use to create all of our service notifications
+// message: the message we want to accompany our notifications (optional)
+// interval: the time from now, in milliseconds, that we want our 
+//           notification displayed (optional) 
 var createNotificationViaService = function(message, interval) {
 	var intent = Ti.Android.createServiceIntent({
         url : 'ExampleService.js'
@@ -13,6 +17,8 @@ var win = Ti.UI.createWindow({
 	backgroundColor: '#222',
 	layout: 'vertical'
 });
+
+// Create button for launching an immedaite notification
 var button = Ti.UI.createButton({
 	title: 'Immediate Notification',
 	height: '50dp',
@@ -23,6 +29,8 @@ button.addEventListener('click', function(e) {
 	createNotificationViaService('Fired immediate notification!');
 });
 
+// Create button for launching a notification that will be launched
+// 4 seconds after we create it.
 var button2 = Ti.UI.createButton({
 	title: 'Pending Notification (4 sec)',
 	height: '50dp',
@@ -45,11 +53,13 @@ var gpsLabel = Ti.UI.createLabel({
 	top: '30dp'
 });
 
+// Create a notification on orientation changes
 Ti.Gesture.addEventListener('orientationchange', function(e) {
 	createNotificationViaService('Orientation changed!');
 });
 
-//Ti.Geolocation.getCurrentPosition(function(e) {});
+// Create a notification whenever we receive location data. This will 
+// work both in the foreground and background.
 if (Ti.Geolocation.locationServicesEnabled) {
     Ti.Geolocation.addEventListener('location', function(e) {
         if (!e.error) {
@@ -59,6 +69,7 @@ if (Ti.Geolocation.locationServicesEnabled) {
         }
     });
 } 
+//Ti.Geolocation.getCurrentPosition(function(e) {});
 
 win.add(button);
 win.add(button2);
